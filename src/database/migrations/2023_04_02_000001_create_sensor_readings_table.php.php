3aa90d72a81ad26b4ae6a202b7ee7bafcec6e8fb
jebbyk <zaycev@addamant.ru>
2023-04-02 13:20:45 +0600

    feat: add migrations

---
+++ src/database/migrations/2023_04_02_000001_create_sensor_readings_table.php.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensor_readings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('sensor_uuid')->constrained('sensors', 'uuid')->cascadeOnDelete();
+            $table->integer('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensor_readings');
+    }
+};
